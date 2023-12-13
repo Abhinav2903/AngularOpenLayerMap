0be2574e545c88a74c6b1ec8afe89d65c993424e
Abhinav2903 <abhinavpareek532@gmail.com>
2023-12-13 23:13:41 +0100

    added wms layer

--- openLayerMap-app/src/app/feature/map-visualisation/map-visualisation.component.ts
+++ openLayerMap-app/src/app/feature/map-visualisation/map-visualisation.component.ts
@@ -7,14 +7,15 @@ import Overlay from 'ol/Overlay';
 import { HttpClient } from '@angular/common/http';
 import Point from 'ol/geom/Point';
 import { Vector as VectorLayer } from 'ol/layer';
-import { Vector as VectorSource } from 'ol/source';
-import { Style, Icon } from 'ol/style';
+import { TileWMS, Vector as VectorSource } from 'ol/source';
+import { Style, Icon, Fill, Stroke } from 'ol/style';
 import Feature from 'ol/Feature';
 import LayerGroup from 'ol/layer/Group';
 import { toStringHDMS } from 'ol/coordinate';
 import { ReverseGeocodeService } from 'src/app/service/reverse-geocode.service';
 import { Observable, catchError, map, of } from 'rxjs';
-
+import GeoJSON from 'ol/format/GeoJSON';
+import { Geometry } from 'ol/geom';
 @Component({
   selector: 'app-map-visualisation',
   templateUrl: './map-visualisation.component.html',
@@ -24,10 +25,17 @@ export class MapVisualisationComponent implements OnInit {
   @ViewChild('map', { static: true }) mapElement!: ElementRef;
   private map!: Map;
   private popup!: Overlay;
+  // private geoJsonLayer: VectorLayer;
+  private geoJsonSource!: VectorSource;
+
   lat: any;
   lng: any;
   address: any;
   initialcord: any;
+  vectorLayer!: VectorLayer<VectorSource<Feature<Geometry>>>;
+  wmsLayerVisible: boolean = false;
+  wmsLayerGroup!: LayerGroup;
+  initialCoordinate=[7.083979689042522, 50.73784467220557];// Eifel Str. 20, Bonn, 53119, Germany
 
   constructor(private reverseGeocodeService: ReverseGeocodeService) {}
 
@@ -37,22 +45,53 @@ export class MapVisualisationComponent implements OnInit {
   }
 
   private initMap(): void {
-    //  this.initialcord = await this.getGeocode('Eifel Str. 20, Bonn, 53119, Germany')
-    // this.lat = this.initialcord.lat;
-    // this.lng = this.initialcord.lng;
     const osmLayer = new TileLayer({
       source: new OSM(),
     });
 
+    this.geoJsonSource = new VectorSource({
+      format: new GeoJSON(),
+    });
+
+    const geoJsonLayer = new VectorLayer({
+      source: this.geoJsonSource,
+      style: this.createStyleFunction(),
+    });
+
+    // / Store a reference to the VectorLayer
+    this.vectorLayer = new VectorLayer({
+      source: this.geoJsonSource,
+      style: this.createStyleFunction(),
+    });
+
+    // Create a layer group for the WMS layer
+    this.wmsLayerGroup = new LayerGroup({
+      layers: [
+        new TileLayer({
+          visible: this.wmsLayerVisible,
+          source: new TileWMS({
+            url: 'https://sedac.ciesin.columbia.edu/geoserver/wms',
+            params: {
+              'LAYERS': 'epi:epi-environmental-performance-index-2020_eco-agriculture',
+              'TILED': true,
+            },
+            serverType: 'geoserver',
+            transition: 0,
+          }),
+        }),
+      ],
+    });
+    
     this.map = new Map({
       target: this.mapElement.nativeElement,
-      layers: [osmLayer],
+      layers: [osmLayer, geoJsonLayer, this.vectorLayer,this.wmsLayerGroup],
       view: new View({
-        center: fromLonLat([7.083979689042522, 50.73784467220557]), // Eifel Str. 20, Bonn, 53119, Germany
+        center: fromLonLat(this.initialCoordinate), 
         zoom: 20,
+        projection: 'EPSG:3857',
       }),
     });
-    this.addMarker([7.083979689042522, 50.73784467220557]);
+    this.addMarker(this.initialCoordinate);
   }
 
   private addMarker(coordinates: number[]): void {
@@ -136,15 +175,80 @@ export class MapVisualisationComponent implements OnInit {
     );
   }
 
-  private getGeocode(address: string) {
-    // this.reverseGeocodeService.geocode(address).subscribe(
-    //   (data)=>{console.log("Data",data.results[0].geometry)})
-    return this.reverseGeocodeService.geocode(address).pipe(
-      map((data) => data.results[0].geometry),
-      catchError((error) => {
-        console.error('Error:', error);
-        return of('Error retrieving address'); // Return a default value or handle the error accordingly
-      })
-    );
+  private createStyleFunction(): Style {
+    return new Style({
+      fill: new Fill({
+        color: 'rgba(255, 0, 0, 0.2)',
+      }),
+      stroke: new Stroke({
+        color: 'rgba(255, 0, 0, 0.8)',
+        width: 2,
+      }),
+    });
+  }
+
+  private loadGeoJsonData(data: any): void {
+    try {
+      console.log('Received GeoJSON data:', data);
+
+      // Create a GeoJSON format object
+      const geoJsonFormat = new GeoJSON();
+
+      // Read features from GeoJSON data
+      const features = geoJsonFormat.readFeatures(data) as Feature<Geometry>[];
+
+      console.log('Parsed GeoJSON features:', features);
+
+      // Clear existing features and add the new features
+      this.geoJsonSource.clear();
+      this.geoJsonSource.addFeatures(features);
+
+      // Zoom to the extent of the features
+      this.map.getView().fit(this.geoJsonSource.getExtent(), {
+        padding: [10, 10, 10, 10],
+        maxZoom: 15, // Adjust the maximum zoom level as needed
+      });
+
+      this.vectorLayer.changed(); // Trigger a layer update
+    } catch (error) {
+      console.error('Error loading GeoJSON data:', error);
+    }
+  }
+
+  onFileInputChange(event: any): void {
+    const file = event.target.files[0];
+    if (file) {
+      const reader = new FileReader();
+      reader.onload = (e: any) => {
+        try {
+          const geoJsonData = JSON.parse(e.target.result);
+          this.loadGeoJsonData(geoJsonData);
+        } catch (error) {
+          console.error('Error parsing GeoJSON file:', error);
+        }
+      };
+      reader.readAsText(file);
+    }
+  }
+  // private getGeocode(address: string) {
+  //   // this.reverseGeocodeService.geocode(address).subscribe(
+  //   //   (data)=>{console.log("Data",data.results[0].geometry)})
+  //   return this.reverseGeocodeService.geocode(address).pipe(
+  //     map((data) => data.results[0].geometry),
+  //     catchError((error) => {
+  //       console.error('Error:', error);
+  //       return of('Error retrieving address'); // Return a default value or handle the error accordingly
+  //     })
+  //   );
+  // }
+
+  // Function to toggle the visibility of the WMS layer
+  toggleWMSLayer(): void {
+    this.wmsLayerVisible = !this.wmsLayerVisible;
+    this.wmsLayerGroup.getLayers().forEach((layer) => {
+      if (layer instanceof TileLayer) {
+        layer.setVisible(this.wmsLayerVisible);
+      }
+    });
   }
 }
